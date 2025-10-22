a187339c53b8519136f5664801e45c3a3b226c0c
K3yZz <1044094@oside.us>
2025-10-22 13:03:37 -0700

    fixiawhdiawh

--- mainpage/main.js
+++ mainpage/main.js
@@ -1,4 +1,4 @@
-import { animateText } from "/globalassets/js/textEffects.js";
+import { animateText } from "./globalassets/js/textEffects.js";
 emailjs.init("GahAe28-OZFZT7Z4F");
 
 if (localStorage.getItem("suppressAlerts") === "true") {
