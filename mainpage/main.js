7fa90afba88bcf589fab552b0368b2404ed1e8e0
K3yZz <1044094@oside.us>
2025-10-23 11:03:51 -0700

    #7

--- mainpage/main.js
+++ mainpage/main.js
@@ -1,4 +1,4 @@
-import { animateText } from "K3yZz/K3ysPlace/globalassets/js/textEffects.js";
+import { animateText } from "../globalassets/js/textEffects.js";
 emailjs.init("GahAe28-OZFZT7Z4F");
 
 function annoyingDebug() {
